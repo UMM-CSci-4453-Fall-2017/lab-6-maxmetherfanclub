fa00e93135e66734074af93bdda5aac7995f54fa
Xaitheng Yang <yang3792@reliant.morris.umn.edu>
2017-10-09 16:45:19 -0500

    progress on summarize-db.js added foundation code for allowing it to view multiple databases currently having issues using field[0].name

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -7,8 +7,12 @@ var mysql=require("mysql");
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
 
+var storedDB = null;
 var storedRows = null;
 var str1 = "describe ";
+var str2 = "show tables in ";
+
+var z = 1;
 
 async.series([
 	function(callback) {
@@ -20,36 +24,49 @@ async.series([
    			 console.log("Connected to Database.");
   		}
 		});
-		connection.query('Use XaiMarsh',function(err,rows,fields){
-                  if(err){
-                        console.log('Error looking up databases: '+err);
-                 } else {
-                        console.log('Successfully using database');
-                        }
-                        callback();
-                });
-
-	},
-//	function(callback) {
 //		connection.query('Use XaiMarsh',function(err,rows,fields){
-//		  if(err){
-//    			console.log('Error looking up databases: '+err);
-// 		 } else {
-//    			console.log('Successfully using database');
-//			}
-//			callback();
-//		});
-//	},
+//                  if(err){
+//                        console.log('Error looking up databases: '+err);
+//                 } else {
+//                        console.log('Successfully using database');
+//                        }
+//                        callback();
+//                });
+
+		connection.query('SHOW DATABASES', function(err, rows, fields){
+			if(err){
+				console.log('SHOW DATABASES error '+err);
+			} else {
+				storedDB = rows;
+				for(i = 0; i < rows.length; i++){
+					storedDB[i] = storedDB[i].Database;
+				}
+				console.log(storedDB[1]);
+				//used for debugging
+				//console.log('it saved databases');
+				//console.log(storedDB[1]);
+				//console.log(storedDB[1].Database);
+			}
+			callback();
+		});
+	},
 	function(callback) {
-		connection.query('SHOW TABLES',function(err,rows,fields){
+		connection.query(str2.concat(storedDB[z]),function(err,rows,fields){
 			if(err){
     				console.log('Error looking up databases2 '+err);
  			 } else {
-    				console.log('it actually went in here');
 				 storedRows = rows;
-				 console.log('it actually went past here');
-				 console.log(storedRows[0].Tables_in_XaiMarsh);
-				 console.log('it actually went farther');
+				 for(i = 0; i < rows.length; i++){
+					 console.log(fields[0].name);
+					 console.log(rows[i].fields[0].name);
+					 console.log(rows[i]);
+					storedRows[i] = rows[i].fields[0].name;
+					 console.log(storedRows[i]);
+				 }
+				 //used for debugging
+				 //console.log('it actually went past here');
+				 //console.log(storedRows[0].Tables_in_XaiMarsh);
+				 //console.log('it actually went farther');
           		}
 			 callback();
 		});
@@ -57,12 +74,13 @@ async.series([
 	function(callback) {
 		var pimple = 0;
 		for (i = 0; i < storedRows.length; i++) {
-			connection.query(str1.concat(storedRows[i].Tables_in_XaiMarsh),function(err,rows,fields) {
+			connection.query(str1.concat(storedDB[z], ".", storedRows[i]),function(err,rows,fields) {
                 		if (err) {
                        			 console.log('Error describing rows'+err);
                			 } else {
-					 console.log(storedRows[pimple]);
-					 console.log('......|XaiMarsh.'+storedRows[pimple].Tables_in_XaiMarsh + '>'); 
+					 //used for debugging
+					 //console.log(storedRows[pimple]);
+					 console.log('......|XaiMarsh.'+storedRows[pimple] + '>'); 
 					 pimple++;
 					 for (j = 0; j < rows.length; j++ ) {
                        			 	console.log(rows[j].Field + " " + rows[j].Type);
